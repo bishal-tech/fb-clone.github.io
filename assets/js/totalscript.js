b22f26a3843a82cdc1a6ba3aa0b4af420b9f45ec
Bishal Debsharma <bishal.tech.com@gmail.com>
2022-04-09 21:33:23 +0530

    fix some css issues

--- assets/js/totalscript.js
+++ assets/js/totalscript.js
@@ -1,38 +1,37 @@
 $(document).ready(function () {
-    //Video section slider here
-    $(".st_slider").slick({
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      arrows: false,
-      dots: false,
-      speed: 300,
-      slidesToScroll: 1,
-      pauseOnHover: true,
-      centerMode: false,
-      centerPadding: "0px",
-      slidesToShow: 3,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: false,
-            centerPadding: "0px",
-            slidesToShow: 3,
-          },
+  /// story section slider
+  $(".st_slider").slick({
+    infinite: false,
+    autoplay: false,
+    autoplaySpeed: 2500,
+    arrows: false,
+    dots: false,
+    speed: 300,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    centerMode: false,
+    centerPadding: "0px",
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: "0px",
+          slidesToShow: 3,
         },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: false,
-            centerPadding: "0px",
-            slidesToShow: 2,
-            slidesToShow: 1,
-          },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: "0px",
+          slidesToShow: 2,
+          slidesToShow: 1,
         },
-      ],
-    });
- 
+      },
+    ],
   });
+});
